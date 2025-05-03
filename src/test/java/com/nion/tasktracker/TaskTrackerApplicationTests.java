584cd48364f2e855d3b3e0ad64160a42d48f7706
Nikon <102325439+MrNikaMilon@users.noreply.github.com>
2025-05-03 14:27:59 +0300

    add security to project and tested them

--- src/test/java/com/nion/tasktracker/TaskTrackerApplicationTests.java
+++ src/test/java/com/nion/tasktracker/TaskTrackerApplicationTests.java
@@ -3,6 +3,8 @@ package com.nion.tasktracker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/// Todo
+/// will added some test on security features and mark test like not in deploy stage only local
 @SpringBootTest
 class TaskTrackerApplicationTests {
 
